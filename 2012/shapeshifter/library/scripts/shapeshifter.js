8b22d5a875540f25d839a0cb2bd3fa29b6611c31
henricavalcante <hnri_mxel@hotmail.com>
2012-10-09 22:10:20 -0300

    _

---
+++ 2012/shapeshifter/library/scripts/shapeshifter.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+					   
+
+	// Activate PrettyPhoto Lightbox handle
+	$("a[rel^='gallery']").prettyPhoto();
+	
+	// PNG Fix
+	$(document).pngFix(); 	
+	
+
+    $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top - 30;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 750);
+       return false;
+      }
+    }
+    });
+
+
+});
